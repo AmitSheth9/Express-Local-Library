502f85a212c0838ff4ae41d61bf650e041bb65b0
AmitSheth9 <amit.sheth9@gmail.com>
2021-12-26 02:23:32 -0800

    made changes prior to deployment

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var catalogRouter = require('./routes/catalog')
 var app = express();
 
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://amit:library@cluster0.oguso.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://amit:library@cluster0.oguso.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
